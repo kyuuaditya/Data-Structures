1ed7c4b08f0eda176f0c81416726cc9a55fbcd90
kyuuaditya <thisisaditya.me@gmail.com>
2024-03-07 10:59:12 +0530

    queues done

--- queues/main.cpp
+++ queues/main.cpp
@@ -9,32 +9,33 @@ struct Node{
 void push(struct Node** head, int node_data,int q){
   struct Node* last=*head;
   struct Node* nei=*head;
-  while(last!=NULL){
-    q--;
-    last=last->next;
-  }
-  if(last->next==NULL&&q>0){
-    struct Node* newNode=new Node;
-    newNode->data=node_data;
-    newNode->next=NULL;
-    last->next=newNode;
-  }
-  if(last->next==NULL&&q==0){
-    struct Node* newNode=new Node;
-    newNode->data=node_data;
-    newNode->next=NULL;
-    last->next=newNode;
-    *head=nei->next;
-  }
+  if(*head==NULL){
+  struct Node* newNode= new Node;
+  newNode->data=node_data;
+  newNode->next=*head;
+  *head=newNode;
+  }else{
+    while(last->next!=NULL){
+      q--;
+      last=last->next;
+    }
+      struct Node* newNode=new Node;
+      newNode->data=node_data;
+      newNode->next=NULL;
+      last->next=newNode;
+    if(q==1){
+      **head=*(nei->next);
+    }
+  } 
 }
 
 void display(struct Node*node){
   while(node->next!=NULL){
-    cout<<node->data<<"-->";
+    std::cout<<node->data<<"<--";
     node=node->next;
   }
   if(node->next==NULL){
-    cout<<"node->data";
+    cout<<node->data;
   }
 }
 
@@ -42,5 +43,9 @@ int main(){
   int q=0;
   struct Node*head=NULL;
   push(&head,8,3);
+  push(&head,9,3);
+  push(&head,1,3);
+  push(&head,0,3);
+  push(&head,19,3);
   display(head);
 }
